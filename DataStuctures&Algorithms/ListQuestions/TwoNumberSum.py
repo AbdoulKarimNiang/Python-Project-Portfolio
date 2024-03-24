d17a8568e093779e14a6435305fab4806c630094
Abdoul Karim Niang <132589713+AbdoulKarimNiang@users.noreply.github.com>
2024-03-24 09:18:12 +0100

    Update TwoNumberSum.py

--- DataStuctures&Algorithms/ListQuestions/TwoNumberSum.py
+++ DataStuctures&Algorithms/ListQuestions/TwoNumberSum.py
@@ -1,3 +1,47 @@
+# Best Approach
+
+from typing import List
+
+numbers = [1, 5, 8, 4, 9, 3, 18, 2]
+target = 22
+
+class Solution:
+    def twoSum(self, nums: List, target: int) -> List:
+        my_value_dictio = {}
+        for i, num in enumerate(nums):
+            compliment = target - num
+            if compliment in my_value_dictio:
+                return [i, my_value_dictio[compliment]]
+            else:
+                my_value_dictio[num] = i
+
+s1 = Solution()
+print(s1.twoSum(numbers,target))
+
+
+
+# Good Aproach
+
+ from typing import List
+ class Solution:
+     def twoSum(self, nums: List[int], target: int) -> List[int]:
+         new_nums = nums.copy()
+         new_nums.sort()
+         i = 0
+         j = len(nums) - 1
+         while (i < j):
+             if(new_nums[i] + new_nums[j] == target):
+                 if (new_nums[i] == new_nums[j]):
+                 	return [nums.index(new_nums[i]),nums.index(new_nums[j],nums.index(new_nums[i])+1)]
+                 else:
+                 	return [nums.index(new_nums[i]),nums.index(new_nums[j])]
+             elif (new_nums[i] + new_nums[j] < target):
+             	i = i + 1
+             else:
+             	j = j - 1
+ s1 = Solution()
+ s1.twoSum([1,5,8,4,9,3,18,2],27)
+
 # Brute Force
 numbers = [1, 5, 8, 4, 9, 3, 18, 2]
 
@@ -11,3 +55,4 @@ for i in range(0, lunghezza):
             print([i, y])
         else:
             pass
+
