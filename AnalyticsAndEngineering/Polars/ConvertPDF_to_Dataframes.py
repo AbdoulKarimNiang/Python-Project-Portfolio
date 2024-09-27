4d4fa33441ae6a0a1b3fb526a16e0bb8899ec7fd
Abdoul Karim <karimniang9@gmail.com>
2024-09-27 12:23:24 +0200

    Solved convertion problem ConvertPDF file

--- AnalyticsAndEngineering/Polars/ConvertPDF_to_Dataframes.py
+++ AnalyticsAndEngineering/Polars/ConvertPDF_to_Dataframes.py
@@ -9,11 +9,10 @@ base_path = os.path.expanduser("~")
 path = os.path.join(base_path,"Downloads", file)
 
 
-df_list = tabula.read_pdf(path, pages='1-2,4-6,8-14,16-23', stream=True, pandas_options={'header': True})
+df_list = tabula.read_pdf(path, pages='1-2,4-6,8-14,16-23', stream=True,
+                          pandas_options={'header': True})
 
-
-
-def process_dtaframe(df):
+def process_dataframe(df):
     """
     The function remove columns with only null values and
     rows with null values.
@@ -28,30 +27,82 @@ def process_dtaframe(df):
     df = df.dropna(axis='columns', how='all')
     df = df.dropna(axis ='index', how='all')
 
-    if df.columns < 5:
+    if len(df.columns) < 5:
         schema = {
             "date_operation": pl.String,
-            "data_valuta": pl.Date,
-            "exits": pl.Float32,
+            "date_valuta": pl.String,
+            "spendings": pl.String,
             'description': pl.String
           }
-    elif df.columns == 5:
+
+        df_polars = pl.DataFrame(df, schema =schema)
+
+        df_polars = df_polars.with_columns(incomes=pl.lit("0").cast(pl.Int8))
+
+    elif len(df.columns) == 5:
         schema = {
             "date_operation": pl.String,
-            "data_valuta": pl.Date,
-            "exits": pl.Float32,
-            "incomes": pl.Float32,
+            "date_valuta": pl.String,
+            "spendings": pl.String,
+            "incomes": pl.String,
             'description': pl.String
         }
 
-    df = pl.from_pandas(df, schema_overrides=schema)
+        df_polars = pl.DataFrame(df, schema =schema)
+
+    df_polars = df_polars.filter(~pl.col("description").is_null())
+
+    df_polars= df_polars.with_columns(
+        spendings=pl.col("spendings")
+            .cast(pl.Utf8)
+            .str.replace_all(r"\.", "")  # Remove thousands separators (periods)
+            .str.replace(",", ".")  # Replace comma with a period (decimal point)
+            .str.replace_all(r"[^\d.]", "").cast(pl.Float32),
+        incomes=pl.col("incomes").cast(pl.Utf8)
+            .str.replace_all(r"\.", "")  # Remove thousands separators (periods)
+            .str.replace(",", ".")  # Replace comma with a period (decimal point)
+            .str.replace_all(r"[^\d.]", "").cast(pl.Float32)
+    )
+
+    df_polars = df_polars.with_columns(
+        pl.col("date_operation").fill_null(strategy='forward'),
+        pl.col("date_valuta").fill_null(strategy='forward'),
+        pl.col("spendings").fill_null(value=0),
+        pl.col('incomes').fill_null(value=0)
+    )
+
+    df_polars =df_polars.group_by("date_operation", "date_valuta").agg(
+        pl.col("spendings"),
+        pl.col("incomes"),
+        pl.col("description").str.concat(delimiter=" ")
+    )
 
 
+    df_polars = df_polars.explode("spendings","incomes")
+
+    # Cast "spendings" and "incomes" to Float32 to allow numeric operations
+    df_polars = df_polars.with_columns(
+        pl.col("spendings").cast(pl.Float32),
+        pl.col("incomes").cast(pl.Float32)
+    )
+
+   # df_polars = df_polars.filter(
+    #    pl.col("spendings") != 0 & pl.col("incomes") != 0
+    #)
 
 
-df = pl.DataFrame()
+    #df_polars = df_polars.with_columns(
+     #   date_operation=pl.col("date_operation").str.strptime(pl.Date,format="%d/%m/Y"),
+      #  date_valuta=pl.col("date_valuta").str.strptime(pl.Date,format="%d/%m/Y"),
+       # incomes=pl.col("incomes").str.extract(f"d+"),
+        #spendings=pl.col("incomes").str.extract(f"d+")
+    #)
 
-polars_dataframes = [pl.from_pandas(df_list[i], schema_overrides=schema) for i in range(1, len(df_list))]
+    # df_polars = df.filter(pl.col("dat"))
+
+    return df_polars
+
+polars_dataframes = [process_dataframe(df_list[i]) for i in range(1, len(df_list))]
 
 print(polars_dataframes)
 
