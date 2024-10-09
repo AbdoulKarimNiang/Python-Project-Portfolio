4932aae011dd16168ad67603c68c75f560c99f72
Abdoul Karim <karimniang9@gmail.com>
2024-10-09 15:00:58 +0200

    Cleaning duplica folders from parent folder Python-projects

--- AnalyticsAndEngineering/Polars/Globant/PythonScript/project_info.py
+++
@@ -1,6 +0,0 @@
-
-host_name = 'localhost'
-user_name = 'root'
-password = '****'
-db_name = 'globant_db'
-
--- UsefulFunctions/CountingLetters.py
+++
@@ -1,8 +0,0 @@
-string = f" A very long sentence that I can't recall"
-
-letters = {}
-for char in string:
-    letters.setdefault(char, 0)
-    letters[char] += 1
-
-print(letters)
