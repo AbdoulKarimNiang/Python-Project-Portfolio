5f09a8f12375aeee2401e68801f6a4fd7d754a9e
Abdoul Karim <karimniang9@gmail.com>
2024-12-25 21:09:57 +0100

    fix inner loop again

--- GamesProjects/MaxBid.py
+++ GamesProjects/MaxBid.py
@@ -5,9 +5,13 @@ while any_bidders:
     name = input("What is your name?: ")
     bid = input("What's your bid?: ")
     dictionary[name] = bid
-    any_bidders = input("Are there any other bidders ('Yes/No'):\n").lower()
+    
+    any_bidders = None
+    while any_bidders not in ('yes', 'no'):
+        any_bidders = input("Are there any other bidders ('Yes/No'):\n").lower()
 
-    if any_bidders == "no":
-        any_bidders = False
+        if any_bidders == "no":
+            any_bidders = False
+            break
 
 print(f"The winner is {max(dictionary,key=dictionary.get)} with a bid of ${max(dictionary.values())}")
