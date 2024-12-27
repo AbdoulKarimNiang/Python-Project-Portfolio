ca670dc6b0dbc2a26cd0922bf41fd011c11176c8
Abdoul Karim <karimniang9@gmail.com>
2024-12-27 16:40:33 +0100

    Fixed Month Calendar

--- 140Projects/8.MonthCalendar.py
+++ 140Projects/8.MonthCalendar.py
@@ -3,13 +3,19 @@ from calendar import month
 year = None
 month_number = None
 
-def get_value(value, name:str, digits_number: int) -> int:
+def get_value(value, name:str, digits: int) -> int:
     while not isinstance(value, (int, float)) :
+    
         try:
-            value: int = int(input(f"Please insert the {name} in {digits_number} digits: "))
+            if name == "month":
+                value: int = int(input(f"Please insert the {name} with up to {digits} digits: "))
+            elif name == "year":
+                value: int = int(input(f"Please insert the {name} in {digits} digits: "))
+            else:
+                continue
         except ValueError as v:
             print(v, "Please insert a valid numeric value")
-            print(f"The value must have exactly {digits_number} digits.")
+            print(f"The value must have exactly {digits} digits.")
         
     return value
 
@@ -17,6 +23,7 @@ year: int = get_value(year, "year",4)
 month_number: int = get_value(month, "month", 2)
 
 while month_number not in range(1,13):
+    print("Months mus be between 1 and 12 inclusive both")
     month_number: int = get_value(month, "month", 2)
 
 
