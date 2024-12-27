9937907d53a081252a3a09e4476655c1191594fe
Abdoul Karim <karimniang9@gmail.com>
2024-12-27 14:22:08 +0100

    Fixed GuessTheNumber.py

--- GamesProjects/GuessTheNumber.py
+++ GamesProjects/GuessTheNumber.py
@@ -13,18 +13,25 @@ def repeat_convert(guess) -> int:
     return guess
 
 
-def check_guessing(player_guess) -> None:
+def check_guessing(player_guess) -> bool:
     """ Check for the correctness of the user guess"""
     if player_guess == number:
         print(f"You got it! The answer was {number}.")
         print(f"The game is finished!")
+        return True
     elif player_guess > number:
         print(f"Too High.")
-        print(f"Guess again.")
+        return False
     elif player_guess < number:
         print(f"Too low.")
-        print(f"Guess again.")
-
+        return False
+    
+def reduce_lives(n_lives: int, correct_guess: bool) -> int:
+    if correct_guess:
+        return n_lives
+    else:
+        n_lives -= 1
+        return n_lives
 
 def game():
     """ This is the Number guessing game"""
@@ -34,30 +41,35 @@ def game():
 
     if difficulty == 'easy':
         lives = 10
-        print(f"You have {lives} attempts to guess the number.")
-        player_guess: str = input(f"Make a guess: ")
-        guess = repeat_convert(player_guess)
-        print(guess)
         for _ in range(1, lives + 1):
-            check_guessing(guess)
-            player_guess: str = input(f"Make a guess: ")
-            guess = repeat_convert(player_guess)
+            if lives >= 0:
+                print(f"You have {lives} attempts to guess the number.")
+                player_guess: str = input(f"Make a guess: ")
+                guess = repeat_convert(player_guess)
+                print(guess)
+                correct_guess = check_guessing(guess)
+                lives = reduce_lives(lives, correct_guess )
+                if correct_guess:
+                    print(f"Congratulation you have guessed the correct number: {number} with {lives} lives left")
+                    break
+            else:
+                print(f"You run out of lives. You lost ❌")
     elif difficulty == 'hard':
         lives = 5
-        print(f"You have {lives} attempts to guess the number.")
-        player_guess: str = input(f"Make a guess: ")
-        guess = repeat_convert(player_guess)
-        print(guess)
         for _ in range(1, lives + 1):
-            check_guessing(guess)
-            player_guess: str = input(f"Make a guess: ")
-            repeat_convert(player_guess)
-            if player_guess != number:
-                lives -= 1
+            if lives >= 0:
                 print(f"You have {lives} attempts to guess the number.")
-            if player_guess == number:
-                break
-            player_guess: int = int(input(f"Make a guess: "))
+                player_guess: str = input(f"Make a guess: ")
+                guess = repeat_convert(player_guess)
+                print(guess)
+                correct_guess = check_guessing(guess)
+                lives = reduce_lives(lives, correct_guess )
+                if correct_guess:
+                    print(f"Congratulation you have guessed the correct number: {number} with {lives} lives left")
+                    break
+            else:
+                print(f"You run out of lives. You lost ❌")
+    
     else:
         print(f"You didn't select the correct difficulty. Try again")
 game()
